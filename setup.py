730146f6bc1ee7e7fe9d5d9f668b3fae5079ac41
kuldeep <poriyakuldeep13@gmail.com>
2023-11-12 12:54:52 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='customer-churn-prediction',
 version='0.0.1',
 author='Kuldeep',
 author_email='poriyakuldeep13@gmail.com',
